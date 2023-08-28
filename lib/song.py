276cfd36c8bb5a033ffe3768f49f3e04488b2adc
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-08-28 11:37:53 +0300

    Completed

--- lib/song.py
+++ lib/song.py
@@ -36,6 +36,29 @@ class Song:
         song.save()
         return song
     
+    @classmethod
+    def get_all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+
+        all = CURSOR.execute(sql).fetchall()
+        cls.all = [cls.new_from_db(row) for row in all]
+    
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM songs
+            WHERE name = ?
+            LIMIT 1
+        """
+
+        song = CURSOR.execute(sql, (name,)).fetchone()
+
+        return cls.new_from_db(song)
+    
 
     
 
